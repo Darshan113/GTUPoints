9b98a94110003ce6b5d28caf84bc22fa86ea3676
jaimin_panchal <jaiminpanchal1208@gmail.com>
2024-03-24 16:42:38 +0530

    create navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'student',loadChildren:()=>import('./pages/layout/layout.module').then(m=>m.LayoutModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

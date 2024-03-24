c6c345ccc634e777505b9f61f7b1587ab14c5c91
Darshan Panchal <dppanchal106@gmail.com>
2024-03-24 17:14:02 +0530

    add links

--- src/app/pages/layout/nav/nav.component.ts
+++ src/app/pages/layout/nav/nav.component.ts
@@ -15,11 +15,10 @@ export class NavComponent implements OnInit {
 
     ngOnInit() {
         this.items = [
-            { label: 'Home', icon: 'pi pi-fw pi-home' },
-            { label: 'Calendar', icon: 'pi pi-fw pi-calendar' },
-            { label: 'Edit', icon: 'pi pi-fw pi-pencil' },
-            { label: 'Documentation', icon: 'pi pi-fw pi-file' },
-            { label: 'Settings', icon: 'pi pi-fw pi-cog' }
+            { label: 'Home', icon: 'pi pi-fw pi-home',routerLink:'home' },
+            { label: 'Add Activity', icon: 'pi pi-plus',routerLink:'add_Activity' },
+            { label: 'Display', icon: 'pi pi-fw pi-pencil',routerLink:'display_Activities' },
+            { label: 'Edit Profile', icon: 'pi pi-user-edit',routerLink:'profile' },
         ];
 
         this.activeItem = this.items[0];
